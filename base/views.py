8c6c0bae3e96a8201df36ca78f3c4daa61af445b
Kyaw Soe Hla <codewarrior.ksh@gmail.com>
2024-11-13 08:27:12 -0800

    add Album and Gallery Model

--- base/views.py
+++ base/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Gallery, Album
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    albums = Album.objects.all()
+    photos = Gallery.objects.all()
+    context = {
+        'albums':albums,
+        'photos':photos,
+    }
+    return render(request, 'index.html',context)
